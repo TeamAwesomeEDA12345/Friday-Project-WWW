363248b477b7ea26e9a6ece91a5a1ac6969917bc
Uili Fecteau & Lauren Colls <pair+Uili-Simteau.lau-ren@devbootcamp.com>
2016-05-06 15:41:34 +1200

    connect gold api

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
+var getWorths = require('../lib/get-worths.js')
 var calculateWorth = require('../lib/calculate-worth.js')
 var saveNewWorth = require('../lib/save-new-worth.js')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // call save to DB function, parse new user object
 
+  getWorths(__dirname + '/../data/database.json', function(err, datafromcalc){
+    if(err) {
+      throw err
+    } else {
 
-  res.render('index', { title: 'Worth Your Weight' });
+     calculateWorth(datafromcalc, function (err, obj){
+      // if (err) {
+      //   throw err
+      // } else {
+        res.render('index', obj);
+      // }
+    })
+   }
+ })
 });
 
 /* POST/UPDATE home page. */
@@ -17,12 +29,12 @@ router.post('/', function(req, res, next) {
 
   //call save New User To DB with params (newUserObj, callback)
   saveNewWorth(__dirname + '/../data/database.json', newUser, function(err, db){
-    console.log(db.worths[0].weight, "this is the thing")
+
       // call the function that calculates worth
       calculateWorth(db, function (err, obj){
         res.render('index', obj);
       })
-  })
+    })
 });
 
 
