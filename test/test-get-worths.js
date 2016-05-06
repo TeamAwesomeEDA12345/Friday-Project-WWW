30043f1d61ab47b1aa67df643ce2b90c261541c5
Oliver Stigley <email": null>
2016-05-06 12:21:34 +1200

    save new worth test passing

--- test/test-get-worths.js
+++ test/test-get-worths.js
@@ -5,11 +5,10 @@ var getWorths = require('../lib/get-worths.js')
 var expected = require('../data/test-database.json')
 
 test("test database is read and returned", function(t){
-  getWorths( function  (err, database) {
+  getWorths(__dirname + "/../data/test-database.json", function  (err, database) {
     if(err) {
       throw err
     } else {
-      console.log(database)
       t.deepEqual(database, expected, "Database is returned")
     }
   })
--- test/test-new-worth.js
+++ test/test-new-worth.js
@@ -8,19 +8,22 @@ var newWorth = {
   "name": "Shayan",
   "weight": 78.0
 }
+var db = {"worths": [{"id": 1, "name": "Olly", "weight": 83.3 },{"id": 2, "name": "Jack", "weight": 76.3 },{"id": 3, "name": "Linda", "weight": 67.0 }]}
+
+//reset database
+fs.writeFile(__dirname + "/../data/test-database.json", JSON.stringify(db), "UTF-8", function afterReset(err){
 
 //set expected length of database
-getWorths( function  (err, database) {
-  console.log("hello")
+getWorths(__dirname + "/../data/test-database.json",  function  (err, database) {
   if(err) {
     throw err
   } else {
-    var expected = 2//database["worths"].length + 1 || 0
+    var expected = database["worths"].length + 1 || 0
 
     //run test adding new item to database, then measuring length and last item
     test('save new worth object to database', function(t){
 
-      saveNewWorth(newWorth, function doThisAfterSaving (err, response, updatedDatabase) {
+      saveNewWorth(__dirname + "/../data/test-database.json", newWorth, function doThisAfterSaving (err, updatedDatabase) {
         if (err) {
           throw err
         } else {
@@ -33,3 +36,9 @@ getWorths( function  (err, database) {
   }
 
 })
+
+
+
+})
+
+
